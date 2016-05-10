fc475ce41fa4b3c1c6d10a915971efdab86ceff5
Jacob Garrison <jgarrison1204@gmail.com>
2016-05-10 15:56:47 -0700

    Create method passes

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,4 +4,14 @@ require 'csv'
 
 class Udacidata
   # Your code goes here!
+  @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
+  def self.create(options={})
+    product_to_add = Product.new(options)
+    #"a+" is from the IO open mode which tells the file to Read-write see  http://ruby-doc.org/core-2.0.0/IO.html#method-c-new-label-IO+Open+Mode
+    CSV.open(@@data_path, "a+") do |csv|
+      csv << [product_to_add.id, product_to_add.brand, product_to_add.name, product_to_add.price]
+    end
+    product_to_add
+  end
 end
