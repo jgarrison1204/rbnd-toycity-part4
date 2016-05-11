73295353507733829f4d1b35937a139cff5f90da
Jacob Garrison <jgarrison1204@gmail.com>
2016-05-11 12:15:41 -0700

    All method passes all tests

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -14,4 +14,15 @@ class Udacidata
     end
     product_to_add
   end
+
+  #sourced from https://discussions.udacity.com/t/expected-id-brand-product-price-to-be-an-instance-of-product-not-array/167823
+  def self.all
+    products = [] #to hold the instances we will create.
+    CSV.read(@@data_path, headers:true).each do |product| #read the CSV and iterate over each row
+      #create a new instance of Product and add it to the products array.
+      products << self.new(id: product["id"], brand: product["brand"], name: product["product"], price: product["price"])
+    end
+    #return the array of products.
+    products
+  end
 end
