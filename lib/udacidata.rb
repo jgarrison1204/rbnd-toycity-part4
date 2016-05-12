c996cdabb36f6b67a26cbff50ffdea3a246f9c0d
Jacob Garrison <jgarrison1204@gmail.com>
2016-05-12 13:01:49 -0700

    Trouble shooting self.first

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -5,12 +5,11 @@ require 'csv'
 class Udacidata
   # Your code goes here!
   @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
-
   def self.create(options={})
     product_to_add = Product.new(options)
     #"a+" is from the IO open mode which tells the file to Read-write see  http://ruby-doc.org/core-2.0.0/IO.html#method-c-new-label-IO+Open+Mode
     CSV.open(@@data_path, "a+") do |csv|
-      csv << [product_to_add.id, product_to_add.brand, product_to_add.name, product_to_add.price]
+      csv << [product_to_add.brand, product_to_add.name, product_to_add.price]
     end
     product_to_add
   end
@@ -24,5 +23,14 @@ class Udacidata
     end
     #return the array of products.
     products
+  end
+
+  #Products array (line 25) is empty we when run puts products.empty?
+  #So calling all in self.first is calling an empty array, which might be cause of nil value in self.first
+  #Not sure how test_all_method_returns_all_products_in_database can pass if products is empty.
+  #How can line 42 in Udacidata_test actual = Product.all.length = !empty and Product.all.length called in self.first be nil?
+
+  def self.first
+    all.first
   end
 end
