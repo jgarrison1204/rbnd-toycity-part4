59256f25f0c3521312a6dd033547027ad7e2e2b7
Jacob Garrison <jgarrison1204@gmail.com>
2016-05-16 18:11:44 -0700

    Used metaprogramming to write find_by methods

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -1,6 +1,19 @@
 class Module
   def create_finder_methods(*attributes)
-    # Your code goes here!
-    # Hint: Remember attr_reader and class_eval
+  # Your code goes here!
+  # Hint: Remember attr_reader and class_eval
+  	attributes.each do |attribute|
+      #Use %Q to interporlate and the create new methods
+  		creates_find_by_brand_or_name = %Q{
+  			def self.find_by_#{attribute}(brand_or_name)
+  				self.all.each do |product|
+  					if product.#{attribute} == brand_or_name
+  						return product
+  					end
+  				end
+  			end
+  		}
+  		class_eval(creates_find_by_brand_or_name)
+  	end
   end
 end
